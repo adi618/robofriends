6cc0152186ef1e45e8de7aaff97b8a280277a1b3
adi618 <59510889+adi618@users.noreply.github.com>
2020-07-25 17:20:58 +0200

    Updates

---
+++ precache-manifest.ea4b92d8a5991ec434aa53f7309a27d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d1fa62dc0050a26cffe6654f171df6a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "d53aef453b22c7ab586c",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "038ab4f15b690d4722a9",
+    "url": "/robofriends/static/css/main.34af555d.chunk.css"
+  },
+  {
+    "revision": "d53aef453b22c7ab586c",
+    "url": "/robofriends/static/js/2.265dc3c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.265dc3c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "038ab4f15b690d4722a9",
+    "url": "/robofriends/static/js/main.4d0e7d8e.chunk.js"
+  },
+  {
+    "revision": "3b0c27274fe20ac069be",
+    "url": "/robofriends/static/js/runtime-main.c688b9fd.js"
+  }
+]);
